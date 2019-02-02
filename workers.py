8c24e464b5edd1e41d2acfb029eb8e5773ce3070
dou <513856032@qq.com>
2019-02-02 11:28:07 +0800

    change tasks

--- workers.py
+++ workers.py
@@ -1,5 +1,5 @@
 from celery import Celery
-app = Celery('crawl_task', include=['tasks'], broker='redis://127.0.0.1:6379/1',backend="redis://127.0.0.1:6379/2")
+app = Celery('crawl_task', include=['tasks'], broker='redis://10.0.8.179:6379/1',backend="redis://10.0.8.179:6379/2")
 # 官方推荐使用json作为消息序列化方式
 app.conf.update(
     CELERY_TIMEZONE='Asia/Shanghai',
